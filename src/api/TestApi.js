7957d21a9e98d68a3c9b1bf7563bfcefd4eb14ce
VanMinhNguyen21 <vanminhnguyen00@gmail.com>
2025-03-02 22:17:47 +0700

    update code

--- src/api/TestApi.js
+++ src/api/TestApi.js
@@ -3,11 +3,11 @@ import { get } from 'react-hook-form';
 
 export const Tests = {
     getTests: async () => {
-        const response = await axios.get('http://localhost:8000/api/exercises');
+        const response = await axios.get('https://nckh-new.vercel.app/api/api/exercises');
         return response.data;
     },
     getQuestionByTestId: async (id) => {
-        const response = await axios.get(`http://localhost:8000/api/question?exercise_id=${id}`);
+        const response = await axios.get(`https://nckh-new.vercel.app/api/api/question?exercise_id=${id}`);
         return response.data;
     }
 };
